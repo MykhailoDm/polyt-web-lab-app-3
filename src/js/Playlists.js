a408ffdf59f5afe664ad17ab55a43f3d7618700e
MykhailoDm <mikhailo2002dm@gmail.com>
2022-06-11 22:11:29 +0300

    Implement playlist creation

--- src/js/Playlists.js
+++ src/js/Playlists.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 function Playlists(props) {
 
   const [pl, setPl] = useState([])
+  const [playlistNameText, setPlaylistNameText] = useState('');
+  const [isPrivate, setIsPrivate] = useState(false);
 
   useEffect(() => {
     loadPlayLists()
@@ -266,6 +268,21 @@ function Playlists(props) {
       return pl;
   }
 
+  function createPlaylist() {
+    fetch('http://127.0.0.1:5000/playlist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${props.basicHeader}`
+      },
+      body: JSON.stringify({
+        playlist_name: playlistNameText,
+        is_private: isPrivate
+      })      
+    }).then(response => response.json())
+    .then(data => setPl(...pl, data));
+  }
+
   return (
       <div className="text-format">
         <h1>Playlists</h1>
@@ -273,6 +290,12 @@ function Playlists(props) {
           return <li>{item.playlist_name}</li>;
         })}
         {props.basicHeader === '' || props.basicHeader === undefined || props.basicHeader === null ? <Link to={'/login'} >Login</Link> : ""}
+        {props.basicHeader === '' || props.basicHeader === undefined || props.basicHeader === null ? "" : (<><form autoComplete='off'>
+        <p>Playlist name</p>
+        <input onChange={event => setPlaylistNameText(event.target.value)} id="user_name_textbox" />
+        <p>Is Private</p>
+        <input type="checkbox" defaultChecked={isPrivate} onChange={() => setIsPrivate(!isPrivate)} />
+      </form><button id="submit_user_btn" onClick={() => createPlaylist()}>Create playlist</button></>)}
       </div>
     );
   }
